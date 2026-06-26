cdc91e5d5830f12ba4320b09733cde2c414b3fe5
Angel Carballeira <angelcarballeira@gmail.com>
2026-06-25 22:32:32 -0300

    feat: add TMDb integration with trendig movies and categories

--- src/main.js
+++ src/main.js
@@ -1,1 +1,80 @@
-import { API_KEY } from "./config";
+import { API_KEY } from './config.js';
+
+async function getTrendingMoviesPreview() {
+  const res = await fetch(
+    'https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY
+  );
+  const data = await res.json();
+
+  const movies = data.results;
+
+  const trendingPreviewMoviesContainer = document.querySelector(
+    '#trendingPreview .trendingPreview-movieList'
+  );
+
+  movies.forEach(movie => {
+    const movieContainer = document.createElement('div');
+    movieContainer.classList.add('movie-container');
+
+    const movieImg = document.createElement('img');
+    movieImg.classList.add('movie-img');
+    movieImg.setAttribute('alt', movie.title);
+    movieImg.setAttribute(
+      'src',
+      'https://image.tmdb.org/t/p/w300' + movie.poster_path
+    );
+
+    movieContainer.appendChild(movieImg);
+    trendingPreviewMoviesContainer.appendChild(movieContainer);
+  });
+}
+
+async function getCategoriesPreview() {
+  const res = await fetch(
+    'https://api.themoviedb.org/3/genre/movie/list?api_key=' + API_KEY
+  );
+  const data = await res.json();
+
+  const categories = data.genres;
+
+  // Container for categories
+  const previewCategoriesContainer = document.querySelector(
+    '#categoriesPreview .categoriesPreview-list'
+  );
+
+  // Colors for categories
+  const colors = [
+    '#ef4444',
+    '#f97316',
+    '#eab308',
+    '#22c55e',
+    '#06b6d4',
+    '#3b82f6',
+    '#6366f1',
+    '#8b5cf6',
+    '#ec4899',
+    '#14b8a6'
+  ];
+
+  categories.forEach((category, index) => {
+
+    const categoryContainer = document.createElement('div');
+    categoryContainer.classList.add('category-container');
+
+    // Color distinto para cada categoría
+    categoryContainer.style.backgroundColor =
+      colors[index % colors.length];
+
+    const categoryTitle = document.createElement('h3');
+    categoryTitle.classList.add('category-title');
+    categoryTitle.setAttribute('id', category.id);
+
+    categoryTitle.textContent = category.name;
+
+    categoryContainer.appendChild(categoryTitle);
+    previewCategoriesContainer.appendChild(categoryContainer);
+  });
+}
+
+getTrendingMoviesPreview();
+getCategoriesPreview();
